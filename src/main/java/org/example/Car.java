1a100311e443ad45d536e04209ab98acff616faf
VesiZaharieva <vesela.zaharieva@abv.bg>
2024-02-25 18:15:24 +0200

    homework

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -1,2 +1,31 @@
-package org.example;public class Car {
-}
+package org.example;
+
+public class Car {
+    public String brand;
+    public String model;
+    private int productionYear;
+    private int currentYear;
+    private double price;
+
+    public Car() {
+        this.brand = "Reno";
+        this.model = "Clio";
+        this.price = 10000;
+        this.productionYear = 2021;
+        this.currentYear = 2024;
+    }
+    public Car(String brand, String model, int currentYear, int productionYear, double price) {
+        this.brand = brand;
+        this.model = model;
+        this.currentYear = currentYear;
+        this.productionYear = productionYear;
+        this.price = price;
+    }
+    public int calculateAge() {
+        return currentYear - productionYear;
+    }
+    public double calculateTax() {
+        return price * 0.05;
+        }
+    }
+
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,23 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double tax;
+        Car car1 = new Car();
+        int age1 = car1.calculateAge();
+        if (age1 <= 5) {
+            tax = 0;
+        } else {
+            tax = car1.calculateTax();
+        }
+        System.out.println("Owed tax for " + car1.brand + " " + car1.model + " is: " + tax);
+
+        Car car2 = new Car ("VW", "Golf", 2024, 2018, 20000);
+        int age2 = car2.calculateAge();
+        if (age2 <= 5) {
+            tax = 0;
+        } else {
+            tax = car2.calculateTax();
+        }
+        System.out.println("Owed tax for " + car2.brand + " " + car2.model + " is: " + tax);
     }
 }
